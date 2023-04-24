ef8fbfe0b1738c40f47c084b3122a56434367d93
Ritwik Banerjee <67327842+Ritwik234@users.noreply.github.com>
2023-04-24 13:20:06 +0530

    Create connec.java

---
+++ connec.java
@@ -0,0 +1,15 @@
+package com.Banking;
+
+import java.sql.*;
+
+public class connec {
+    Connection c;
+    Statement s;
+    public connec(){                    // for attaching sql part before trybut it didn't worked
+     /*   try {
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+    }
+}
